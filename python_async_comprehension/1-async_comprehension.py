61f966bddde4b10c79ae3e0d3e1e97d925c7f5bc
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-04-22 18:07:25 +0200

    Update

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,4 +6,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> list[float]:
     """A coroutine called async_comprehension."""
-    return [i async for i in async_generator()]
+    return [i async for i in async_generator()][:10]
