ef1c4e575922033fdc232e6653097a1256c7cedc
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-04-22 18:18:07 +0200

    Update

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -8,5 +8,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """Returns a asyncio.Task"""
-    loop = asyncio.get_event_loop()
-    return loop.create_task(wait_random(max_delay))
+    return asyncio.create_task(wait_random(max_delay))
