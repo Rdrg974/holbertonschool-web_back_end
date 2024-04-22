63bf6a1b46d303ffa79d2bdbf93d9aea1f1ec1ac
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-04-22 18:12:26 +0200

    Update

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """A coroutine called async_comprehension."""
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """A coroutine called async_comprehension."""
-    return [i async for i in async_generator()][:10]
+    return [i async for i in async_generator()]
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -4,7 +4,7 @@ A measure_runtime coroutine that will execute
 async_comprehension four times in parallel.
 """
 import asyncio
-import timeit
+import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
@@ -13,8 +13,9 @@ async def measure_runtime() -> float:
     """
     Measure the runtime and return it.
     """
-    start = timeit.default_timer()
-    await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    end = timeit.default_timer()
-    total_time = end - start
-    return total_time
+    task = []
+    start = time.time()
+    for _ in range(4):
+        task.append(asyncio.create_task(async_comprehension()))
+    end = time.time()
+    return end - start
