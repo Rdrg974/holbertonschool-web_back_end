172454fd3dfaa5850f7495c3bae7260a62dc8ab4
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-04-22 09:12:16 +0200

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A type-annotated function floor."""
+
+def floor(n: float) -> int:
+    """Returns the floor of the float."""
+    if n < 0:
+        return int(n) + 1
+    return int(n)
