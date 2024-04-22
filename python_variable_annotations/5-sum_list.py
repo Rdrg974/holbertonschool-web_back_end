bf54ab1307341f1fa3b26f0ca8a261ba21943f40
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-04-22 09:36:46 +0200

    Update

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """A type-annotated function sum_list."""
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Returns their sum as a float."""
     sum_float = 0.0
     for item in input_list:
