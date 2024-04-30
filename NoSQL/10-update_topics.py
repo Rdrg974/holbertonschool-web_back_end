c9237e8cdf6b646b1574d29ee53abc7ab66dffc4
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-04-30 05:18:20 +0200

    Change school topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Changes all topics of a school document based on the name"""
+
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """Changes all topics of a school document based on the name."""
+    mongo_collection.update_many({'name': name}, {'$set': {'topics': topics}})
