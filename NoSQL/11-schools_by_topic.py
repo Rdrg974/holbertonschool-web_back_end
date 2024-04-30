6ec1b2535794ead52b1baa7abe6fa7e91f40ca95
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-04-30 05:21:39 +0200

    Where can I learn Python?

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Returns the list of school having a specific topic."""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Returns the list of school having a specific topic."""
+    return mongo_collection.find({"topics": topic})
