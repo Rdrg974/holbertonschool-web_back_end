dc02eb63def9909e30dffca4a74772bae2c75b5d
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-05-11 15:17:30 +0200

    A Building

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
