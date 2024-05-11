20601f4e212cc8f347a8284fd171214910bc5679
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-05-11 15:44:31 +0200

    Fix code

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
