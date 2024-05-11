a2f6676e27f23ffc9cad0ed0dacf7b68bf99f81e
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-05-11 15:17:42 +0200

    Inheritance

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,16 @@
+import Buiding from './5-building';
+
+export default class SkyHighBuilding extends Buiding {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
