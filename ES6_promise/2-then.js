5ccc9ced8f60e5d3c579f78399d6131ea45c13ef
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-05-14 02:51:39 +0200

    Catch me if you can!

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,4 @@
+export default function handleResponseFromAPI(promise) {
+  return promise.then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => Error()).finally(() => console.log('Got a response from the API'));
+}
